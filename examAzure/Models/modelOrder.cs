1620d2ce0c9275a28a479beff29e3ea849e2d86f
Bodoi <109707645+kieuduc123@users.noreply.github.com>
2023-11-24 16:05:03 +0700

    Baithi_Azue

---
+++ examAzure/Models/modelOrder.cs
@@ -0,0 +1,30 @@
+﻿namespace examAzure.Models
+{
+    public class modelOrder
+    {
+        public string itemCode { get; set; }
+
+        public string ItemName { get; set; }
+
+        public int ItemQty { get; set; }
+
+        public string OrderAddress { get; set; }
+
+        public string PhoneNumber { get; set; }
+    }
+
+
+    public class modelOrder2
+    {
+
+        public string ItemName { get; set; }
+
+        public int ItemQty { get; set; }
+
+        public string OrderAddress { get; set; }
+
+        public string PhoneNumber { get; set; }
+    }
+
+
+}
